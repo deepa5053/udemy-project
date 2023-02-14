9bd7326bfbe109580fe6637eb684b463ffa908b2
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-02-14 16:03:04 +0100

    basic form created

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,18 +6,22 @@ import Button from "../UI/Button";
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
-        event.preventDefault();
+      event.preventDefault();
     };
-    return(
-    <Card className={classes.input}>
-    <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <Button type="submit">Add User</Button>
-    </form>
-    </Card>
+  
+    return (
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
     );
-    }
-export default AddUser;
+  };
+  
+  export default AddUser;
+
+  
